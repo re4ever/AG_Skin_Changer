a0ac84ef6c28d2cb79d690d6728e0363277ccc7f
re4ever <re4ever@naver.com>
2022-01-02 21:28:30 +0900

    Resource File Update

--- 0_CopyData.bat
+++ 0_CopyData.bat
@@ -1,2 +1,8 @@
+del /S /Q .\Result
+
 copy ..\AG_Translate_new\Source\CN\8b2d33babe319cce080b823fe0311ef7 .\8b2d33babe319cce080b823fe0311ef7
 copy ..\AG_Translate_new\Source\CN\0ee69f7dbb2d3f38a9a4ca4f937567ac .\0ee69f7dbb2d3f38a9a4ca4f937567ac
+copy ..\AG_ReplaceImg\CN\spineag_*_resfight .\Source\spineag_*_resfight
+copy ..\AG_ReplaceImg\CN\spineag_*_rescommon .\Source\spineag_*_rescommon
+copy ..\AG_ReplaceImg\CN\spineag_*_uiskel .\Source\spineag_*_uiskel
+dir/b .\Source\spineag_* > ResourceSource.txt
---
+++ 1_Execute.bat
@@ -0,0 +1,1 @@
+.\AGSkinChanger.exe .\ResourceSource.txt
