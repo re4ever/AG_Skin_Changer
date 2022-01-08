136b59080ec2fc423836af47fc4578cfe42b280c
re4ever <re4ever@naver.com>
2022-01-08 17:56:24 +0900

    Skill Video 추가

--- 0_CopyData.bat
+++ 0_CopyData.bat
@@ -2,6 +2,7 @@ del /S /Q .\Result
 
 copy ..\AG_Translate_new\Source\CN\8b2d33babe319cce080b823fe0311ef7 .\8b2d33babe319cce080b823fe0311ef7
 copy ..\AG_Translate_new\Source\CN\0ee69f7dbb2d3f38a9a4ca4f937567ac .\0ee69f7dbb2d3f38a9a4ca4f937567ac
+copy ..\AG_Translate_new\Source\CN\5e9ab432a5f876985a6823e27b619540 .\5e9ab432a5f876985a6823e27b619540
 copy ..\AG_ReplaceImg\CN\spineag_*_resfight .\Source\spineag_*_resfight
 copy ..\AG_ReplaceImg\CN\spineag_*_rescommon .\Source\spineag_*_rescommon
 copy ..\AG_ReplaceImg\CN\spineag_*_uiskel .\Source\spineag_*_uiskel
