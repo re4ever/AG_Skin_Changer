517fa11b15c3bd42e5af12d84b11cfa1b1371572
re4ever <re4ever@naver.com>
2022-07-04 11:20:58 +0900

    update

--- 0_CopyData.bat
+++ 0_CopyData.bat
@@ -1,9 +1,13 @@
 del /S /Q .\Resources
+del /S /Q .\Source
 
-copy ..\AG_Translate_new\Source\CN\8b2d33babe319cce080b823fe0311ef7 .\8b2d33babe319cce080b823fe0311ef7
-copy ..\AG_Translate_new\Source\CN\0ee69f7dbb2d3f38a9a4ca4f937567ac .\0ee69f7dbb2d3f38a9a4ca4f937567ac
-copy ..\AG_Translate_new\Source\CN\5e9ab432a5f876985a6823e27b619540 .\5e9ab432a5f876985a6823e27b619540
-copy ..\AG_ReplaceImg\CN\spineag_*_resfight .\Source\spineag_*_resfight
-copy ..\AG_ReplaceImg\CN\spineag_*_rescommon .\Source\spineag_*_rescommon
-copy ..\AG_ReplaceImg\CN\spineag_*_uiskel .\Source\spineag_*_uiskel
+copy /Y ..\AG_Translate_new\Source\CN\8b2d33babe319cce080b823fe0311ef7 .\8b2d33babe319cce080b823fe0311ef7
+copy /Y ..\AG_Translate_new\Source\CN\0ee69f7dbb2d3f38a9a4ca4f937567ac .\0ee69f7dbb2d3f38a9a4ca4f937567ac
+copy /Y ..\AG_Translate_new\Source\CN\5e9ab432a5f876985a6823e27b619540 .\5e9ab432a5f876985a6823e27b619540
+copy /Y ..\AG_ReplaceImg\CN\spineag_*_resfight .\Source\spineag_*_resfight
+copy /Y ..\AG_ReplaceImg\CN\spineag_*_rescommon .\Source\spineag_*_rescommon
+copy /Y ..\AG_ReplaceImg\CN\spineag_*_uiskel .\Source\spineag_*_uiskel
+copy /Y ..\AG_ReplaceImg\Result\Char\spineag_*_resfight .\Source\spineag_*_resfight
+copy /Y ..\AG_ReplaceImg\Result\Char\spineag_*_rescommon .\Source\spineag_*_rescommon
+copy /Y ..\AG_ReplaceImg\Result\Char\spineag_*_uiskel .\Source\spineag_*_uiskel
 dir/b .\Source\spineag_* > ResourceSource.txt
